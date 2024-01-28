2ecdf827a9928c50240f8e1d88ee7f6520fa895d
Pavel Dobias <dobias.parez@gmail.com>
2024-01-28 13:47:20 +0000

    added menu

--- Laughsylum/Assets/MainMenu.cs
+++ Laughsylum/Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void Quit()
